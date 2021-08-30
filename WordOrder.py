375f641079f183ac9acdc2ae7253c9c54687608a
Ryan Lamb <61068406+rclamb27@users.noreply.github.com>
2021-08-30 09:55:43 -0700

    Create WordOrder.py

---
+++ WordOrder.py
@@ -0,0 +1,6 @@
+from collections import Counter, OrderedDict
+class OrderedCounter(Counter, OrderedDict):
+    pass
+d = OrderedCounter(input() for i in range(int(input())))
+print(len(d))
+print(*d.values())
