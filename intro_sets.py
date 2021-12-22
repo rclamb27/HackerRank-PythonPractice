0e44f0bd5f9896613d03b41289fe73c93c781664
Ryan Lamb <61068406+rclamb27@users.noreply.github.com>
2021-12-21 23:17:23 -0800

    Create intro_sets.py

---
+++ intro_sets.py
@@ -0,0 +1,7 @@
+def average(array):
+    return (sum(array))/len(array)
+if __name__ == '__main__':
+    n = int(input())
+    arr = list(map(int, input().split()))
+    result = average(arr)
+    print(result)
